28ccc1df48bf40352b90b383a66809e766451982
danielsky81 <karma0rg4u@gmail.com>
2021-04-11 23:00:51 +0100

    Add Sign in and Sign up forms

---
+++ src/components/form-input/form-input.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChage, label, ...otherProps}) => (
+  <div className='group'>
+    <input className='form-input'></input>
+  </div>
+)

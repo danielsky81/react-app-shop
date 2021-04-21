f7d6162bca7c4f504413aa96c4a2c1e758945c54
danielsky81 <karma0rg4u@gmail.com>
2021-04-22 00:30:14 +0100

    Configuration for Firebase and adding a user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,11 +10,36 @@ const config = {
   projectId: "e-commerce-react-app-5c24b",
   storageBucket: "e-commerce-react-app-5c24b.appspot.com",
   messagingSenderId: "918639485898",
-  appId: "1:918639485898:web:67dd496d5790638571e533",
-  measurementId: "G-RMPMK0WEB8"
+  appId: "1:918639485898:web:f76d2bd0392925b471e533",
+  measurementId: "G-K1D1TZSMTX"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

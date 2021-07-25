138588b7f367d06692cdca7e101476fe9d601f45
danielsky81 <karma0rg4u@gmail.com>
2021-07-25 15:47:48 +0100

    Update cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,13 @@
+// Utility functions allow us to keep our files clean and organize functions that we may need in multiple files in one location
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const exisitngCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
+  if (exisitngCartItem) {
+    return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id
+      ? {...cartItem, quantity: cartItem.quantity + 1} 
+      : cartItem
+      )
+  }
+  return [...cartItems, {...cartItemToAdd, quantity: 1}]
+}
